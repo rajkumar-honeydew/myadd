b00e9f9a8f11bdf39d6b152e24cff2880d26a5f7
Rajkumar <rajkumar.ulaganadhan@honeydew-technologies.com>
2015-09-21 18:20:25 +0530

    sec tok

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,1 @@
+Playgrounds::Application.config.secret_key_base = '7e11e6389b240a44b0dd2930507d6c081005d7f1191f54548c2e843c38efadf3b754094c87bccd786f0f0f17e004e2d21fa108a890062978b6ce95d0f1cc1631' # replace this
