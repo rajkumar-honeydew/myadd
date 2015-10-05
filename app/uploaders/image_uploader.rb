dc38e64aa4d4b4ae41e18507f78535afe3d61cc9
prasanna-honeydew <prasanna.venkatesh@honeydew-technologies.com>
2015-10-05 15:56:25 +0530

    fix

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,23 +1,23 @@
 # encoding: utf-8
 
-class ImageUploader < CarrierWave::Uploader::Base
+# class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-   include CarrierWave::RMagick
+  #  include CarrierWave::RMagick
 
-  storage :file
+  # storage :file
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
-  version :thumb do
-    process :resize_to_limit => [200, 200]
-  end
+  # version :thumb do
+  #   process :resize_to_limit => [200, 200]
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -51,4 +51,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
-end
+# end
